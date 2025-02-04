ab17360411aadd2b854cf85b702fe237a1d9d2cb
tim12367 <tim12367@ymail.com>
2025-02-04 15:33:53 +0800

    新增H2測試資料庫

--- src/main/resources/static/util.js
+++ src/main/resources/static/util.js
@@ -1,5 +1,10 @@
 // 刷新access token並重試
 function retryWithRefreshToken(ajaxObj) {
+    if (ajaxObj == null) {
+        console.error("必須傳入要ajax的資料!");
+        return;
+    }
+
     ajaxObj.tryCount++;
     if (ajaxObj.tryCount <= ajaxObj.retryLimit) {
         console.log("retry...");
@@ -17,5 +22,9 @@ function retryWithRefreshToken(ajaxObj) {
 
 // 使用refresh token 重新請求 access token
 function refreshToken() {
-    localStorage.setItem("access_token", "refresh") // TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    const refreshToken = localStorage.getItem("refresh_token");
+    if (refreshToken == null) {
+        console.log("沒有登入紀錄!!請重新登入!");
+    }
+    localStorage.setItem("access_token", "refresh");// TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }

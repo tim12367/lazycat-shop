a20e9e315e1b4fdcdaab9bad27dc67e0ba2703af
tim12367 <tim12367@ymail.com>
2025-02-05 14:13:08 +0800

    add builder

--- src/main/java/uk/lazycat/shop/entity/shop/Authorities.java
+++ src/main/java/uk/lazycat/shop/entity/shop/Authorities.java
@@ -1,23 +1,30 @@
 package uk.lazycat.shop.entity.shop;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Authorities {
-    private String username;
+	private String username;
 
-    private String authority;
+	private String authority;
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getAuthority() {
-        return authority;
-    }
+	public String getAuthority() {
+		return authority;
+	}
 
-    public void setAuthority(String authority) {
-        this.authority = authority;
-    }
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
 }
--- src/main/java/uk/lazycat/shop/entity/shop/Users.java
+++ src/main/java/uk/lazycat/shop/entity/shop/Users.java
@@ -1,33 +1,40 @@
 package uk.lazycat.shop.entity.shop;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
-    private String username;
+	private String username;
 
-    private String password;
+	private String password;
 
-    private Boolean enabled;
+	private Boolean enabled;
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public Boolean getEnabled() {
-        return enabled;
-    }
+	public Boolean getEnabled() {
+		return enabled;
+	}
 
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
 }

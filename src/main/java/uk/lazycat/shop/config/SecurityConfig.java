bb6d28791fe6d140bcc171ea5376fcc14e799de5
tim12367 <tim12367@ymail.com.tw>
2025-02-08 17:17:11 +0800

    新增：spring security

---
+++ src/main/java/uk/lazycat/shop/config/SecurityConfig.java
@@ -0,0 +1,94 @@
+package uk.lazycat.shop.config;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import uk.lazycat.shop.exception.SecurityExceptionHandler;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http, SecurityExceptionHandler handler) throws Exception {
+		// 驗證所有請求
+		http.authorizeHttpRequests((requests) -> {
+			requests
+					.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/*.html", "/*.js").permitAll()
+					.anyRequest().authenticated();
+		});
+
+		// 使用stateless不建立session
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// csrf 停用
+		http.csrf(csrf -> csrf.disable());
+
+		// <frame>啟用
+		http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+
+		// 設定oauth2.0
+		http.oauth2ResourceServer(oauth2ResourceServer -> {
+			oauth2ResourceServer.jwt(Customizer.withDefaults());
+			oauth2ResourceServer.authenticationEntryPoint((request, response, e) -> handler.handleAuthenticationException(request, response, e));
+			oauth2ResourceServer.accessDeniedHandler((request, response, e) -> handler.handleAccessDeniedException(request, response, e));
+		});
+
+		return http.build();
+	}
+
+	@Value("${jwt.public.key}")
+	private RSAPublicKey key; // 載入公鑰
+
+	@Value("${jwt.private.key}")
+	private RSAPrivateKey priv; // 載入私鑰
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); // 預設10
+	}
+
+	@Bean
+	JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withPublicKey(this.key).build();
+	}
+
+	@Bean
+	JwtEncoder jwtEncoder() {
+		JWK jwk = new RSAKey.Builder(this.key).privateKey(this.priv).build();
+		JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+		return new NimbusJwtEncoder(jwks);
+	}
+
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		grantedAuthoritiesConverter.setAuthorityPrefix(""); // 刪除jwt authority prefix
+
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+		return jwtAuthenticationConverter;
+	}
+}

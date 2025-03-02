88c7b540337411c9a744885fce47f822e88ccdba
tim12367 <tim12367@ymail.com>
2025-03-02 18:28:33 +0800

    調整:jwt驗證失敗錯誤訊息

--- src/main/java/uk/lazycat/shop/exception/LazycatStatusCode.java
+++ src/main/java/uk/lazycat/shop/exception/LazycatStatusCode.java
@@ -7,8 +7,10 @@ public enum LazycatStatusCode {
 	SUCCESS("0000", "請求成功"),
 	PARAMETER_ERROR("0001", "請求參數錯誤: %s"),
 	CUSTOM_ERROR("0002", "自定義錯誤訊息"),
+	AUTHENTICATION_EXCEPTION("0003", "TOKEN驗證錯誤"),
+	ACCESS_DENIED("0004", "TOKEN權限被拒"),
 	UNKNOWN_ERROR("9999", "發生非預期錯誤，請聯絡管理員");
-
+	
 	private String code;
 	private String info;
 
--- src/main/java/uk/lazycat/shop/exception/SecurityExceptionHandler.java
+++ src/main/java/uk/lazycat/shop/exception/SecurityExceptionHandler.java
@@ -2,10 +2,14 @@ package uk.lazycat.shop.exception;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,13 +19,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SecurityExceptionHandler {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	public void handleAuthenticationException(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException {
 		log.error("TOKEN驗證錯誤 401", e);
-		response.sendError(HttpStatus.UNAUTHORIZED.value(), "驗證錯誤");
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(getErrorMsg(LazycatStatusCode.AUTHENTICATION_EXCEPTION));
+		response.getWriter().flush();
 	}
 
 	public void handleAccessDeniedException(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException {
 		log.error("TOKEN權限被拒 403", e);
-		response.sendError(HttpStatus.FORBIDDEN.value(), "權限被拒");
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(getErrorMsg(LazycatStatusCode.ACCESS_DENIED));
+		response.getWriter().flush();
+	}
+
+	/*
+	 * 取得回應字串
+	 */
+	private String getErrorMsg(LazycatStatusCode lazycatStatusCode) {
+		ErrorMsg errorMsg = new ErrorMsg();
+		errorMsg.setCode(lazycatStatusCode.getCode());
+		errorMsg.setInfo(lazycatStatusCode.getInfo());
+
+		String message = "";
+		try {
+			return objectMapper.writeValueAsString(errorMsg);
+		} catch (JsonProcessingException e) {
+			log.error("錯誤訊息轉換發生錯誤!", e);
+		}
+
+		return message;
 	}
 }
